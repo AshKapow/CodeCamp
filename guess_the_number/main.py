00ace88fb4207227908ed4aea695c253c5e80a59
Ash Powell <ash@thekapow.com>
2022-05-20 16:17:20 +0100

    Implement computer_guess

--- guess_the_number/main.py
+++ guess_the_number/main.py
@@ -11,5 +11,21 @@ def guess(x):
             print('Sorry guess again. Too high.')
     print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f'Yay, the computer guessed your number {guess}, correctly!')
  
-guess(10)
+computer_guess(10)
